fab0f5c8786beddd5abe3471b7ba67a9ddd3aee8
Juan Ignacio Donoso <jidonoso@gmail.com>
2014-05-22 13:01:26 -0400

    fix(build-tools) Build tools failed on sdk update filter

--- manifests/build_tools.pp
+++ manifests/build_tools.pp
@@ -2,13 +2,15 @@
 #
 # Usage:
 #
-#     android::build_tools { '18.1.1': }
+#     android::build_tools { 'build-tools-18.1.1': }
 define android::build_tools(
   $ensure    = present,
   $version   = $name
 ) {
-  android::definition{ $version:
+  $version_number = regsubst($version, 'build-tools-', '')
+
+  android::definition{ $name:
     ensure    => $ensure,
-    dest_path => "build-tools/${version}",
+    dest_path => "build-tools/${version_number}",
   }
 }
